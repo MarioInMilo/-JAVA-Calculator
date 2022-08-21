26bb8ca4ee90ee97a016db2e6083f6e298baa8ac
Ivan S <marioinmilo@mail.ru>
2022-08-21 21:58:45 +0300

    adding checkInvalidSymbolAndEmptyExp to CalculatorMenu

--- src/CalculatorMenu.java
+++ src/CalculatorMenu.java
@@ -6,6 +6,24 @@ import java.util.ArrayList;
 public class CalculatorMenu {
     private final BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
+    public void Calculator() {
+
+    }
+
+    private void checkInvalidSymbolAndEmptyExp(String text) {
+        if (text.length() == 0) {
+            System.err.println("U DIDN'T ENTER ANYTHING");
+            Calculator();
+        }
+        for (int i = 0; i < text.length(); i++) {
+            char symbol = text.charAt(i);
+            if (!((symbol >= 40 && symbol <= 57 || symbol == 32)
+                    && (symbol != 44 && symbol != 46))) {
+                System.err.println("AN INVALID EXPRESSION IS ENTERED: " + text);
+                Calculator();
+            }
+        }
+    }
 
     private int calcMethod(String text) {
         SymbolSwitcher symbolSwitcher = new SymbolSwitcher();
