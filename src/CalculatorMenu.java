12ced763d1348b22de17840c634063ffd102d252
Ivan S <marioinmilo@mail.ru>
2022-08-21 21:56:21 +0300

    adding queryToInput to CalculatorMenu

--- src/CalculatorMenu.java
+++ src/CalculatorMenu.java
@@ -1,3 +1,17 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class CalculatorMenu {
-    
+    private final BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+    private String queryToInput() {
+        String userText = null;
+        try {
+            userText = reader.readLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return userText;
+    }
 }
